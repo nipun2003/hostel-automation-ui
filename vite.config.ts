e97eee037b447252077df295a39dc5dc05bee648
Nipun Kumar <nipun.kumar@mayadataprivacy.eu>
2024-02-20 00:21:40 +0530

    Rollup option added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import {defineConfig} from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    }
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	rollupOptions: {
+		external: ["react", "react-router", "react-router-dom", "react-redux"],
+		output: {
+			globals: {
+				react: "React",
+			},
+		},
+	},
+	build: {
+		rollupOptions: {
+			external: ["react", "react-router", "react-router-dom", "react-redux"],
+			output: {
+				globals: {
+					react: "React",
+				},
+			},
+		},
+	},
+});
